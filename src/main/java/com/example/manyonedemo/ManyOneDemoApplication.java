be0d958b52f7de839b7582c6226b3d8a2b8c265f
winnaingsoe <winnaingsoe@outlook.com>
2022-12-05 14:08:06 +0630

    application.properties config

---
+++ src/main/java/com/example/manyonedemo/ManyOneDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.manyonedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyOneDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManyOneDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/manyonedemo/ManyOneDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manyonedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyOneDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
